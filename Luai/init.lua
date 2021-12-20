77b52d10f8f70a79e858953028416c0c71978daa
PhoenixWhitefire <86601049+PhoenixWhitefire@users.noreply.github.com>
2021-12-20 10:55:17 +0530

    tokens -> Tokens
    Accidental typo, code defines "Tokens" but later uses "tokens" with a lowercase "t".

--- Luai/init.lua
+++ Luai/init.lua
@@ -1,7 +1,7 @@
 return function (Source : string)
 	local Tokens, ErrorMessage = require(script.Lexer).new(Source):generate_tokens()
 	if Tokens then
-		require(script.Parser).new(tokens):parse()
+		require(script.Parser).new(Tokens):parse()
 	else
 		warn(ErrorMessage)
 	end
